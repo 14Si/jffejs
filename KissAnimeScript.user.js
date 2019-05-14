99fa1dcba74d5c61793503af9422b9daf874d58e
14Si <43591915+14Si@users.noreply.github.com>
2019-05-14 02:13:41 -0700

    Update KissAnimeScript.user.js

--- KissAnimeScript.user.js
+++ KissAnimeScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         KissAnime_Script
+// @name         KissAnime Script
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  hmmmmm
 // @updateURL    github.com/14Si/jffejs/blob/master/KissAnimeScript.user.js
 // @author       Silicon
