06cb265f245a49f5594048440fab1a4268ab1d2e
14Si <43591915+14Si@users.noreply.github.com>
2019-05-19 17:43:31 -0700

    Update KissAnimeScript.user.js

--- KissAnimeScript.user.js
+++ KissAnimeScript.user.js
@@ -30,21 +30,49 @@
   }, 100);
 
 
-    var intro_start_time=-1;
-    var intro_end_time=-1;
-    function video_intro_fn(){
-        if(intro_start_time<video.currentTime){
-            if(video.currentTime<intro_end_time){
-                video.currentTime=intro_end_time;
-                video.removeEventListener("timeupdate",video_intro_fn);
+    function video_skip_fn(skip_start_time,skip_end_time){
+        video.addEventListener('timeupdate',function(){
+            if(skip_start_time<video.currentTime){
+                if(video.currentTime<skip_end_time){
+                    video.currentTime=skip_end_time;
+                    video.removeEventListener('timeupdate',arguments.callee);
+                }
             }
-        }
+        });
+        return;
     }
 
-    if(window.location.href.includes('Mahou-Sensei-Negima')){
-        intro_start_time=0;
-        intro_end_time=90;
-        video.addEventListener("timeupdate",video_intro_fn);
+    function video_skip_fn_inc(skip_start_time,skip_inc_time){
+        video_skip_fn(skip_start_time,skip_start_time+skip_inc_time);
+        return;
+    }
+
+    if(window.location.href.includes('Anime/Mayo-Chiki/')){
+        video_skip_fn(1330.514499,1418.493547);
+        if(window.location.href.includes('Episode-008')){
+            video_skip_fn_inc(97,90);
+        } else if(window.location.href.includes('Episode-009')){
+            video_skip_fn_inc(41,90);
+        }
+    } else if(window.location.href.includes('Anime/Mahou-Sensei-Negima/')){
+        video_skip_fn(0,90);
+    } else if(window.location.href.includes('Anime/Ore-no-Kanojo-to-Osananajimi-ga-Shuraba-Sugiru/')){
+        if(window.location.href.includes('Episode-001')){
+            video_skip_fn_inc(29.537888,90);
+        } else if(window.location.href.includes('Episode-002')){
+            video_skip_fn_inc(102.231999,90);
+        } else if(window.location.href.includes('Episode-003')){
+            video_skip_fn_inc(97.126749,90);
+        } else if(window.location.href.includes('Episode-004')){
+            video_skip_fn_inc(26.249208,90);
+            video_skip_fn_inc(1298.18138,90);
+        } else if(window.location.href.includes('Episode-005')){
+            video_skip_fn_inc(24.298805,90);
+            video_skip_fn_inc(1315.665789,90);
+        } else if(window.location.href.includes('Episode-006')){
+            video_skip_fn_inc(31.35056,90);
+            video_skip_fn_inc(1268.438949,90);
+        }
     }
 
 //     window.alert(testj0.href);
